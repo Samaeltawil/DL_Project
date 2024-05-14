5adf51b647dedae6fe4603cb66eb51c608a3501e
Adrien Pannatier <adrien.pannatier@epfl.ch>
2024-05-14 21:09:27 +0200

    model working for text, need to clean code a bit

--- custom_dataset.py
+++ custom_dataset.py
@@ -4,43 +4,6 @@ from PIL import Image
 import pandas as pd
 from transformers import BertTokenizer
 
-# class MMHS_150KDataset(Dataset):
-#     def __init__(self, GT_path, image_path, transform=None):
-#         self.GT_path = GT_path
-#         self.GT_data = pd.read_csv(GT_path)
-#         self.image_path = image_path
-        
-#         self.transform = transform
-#         self.len_samples = len(self.GT_data.iloc[:, 0])
-#         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-
-#     def __len__(self):
-#         return self.len_samples
-
-#     def __getitem__(self, idx):
-#         ID = self.GT_data.iloc[idx, 0]
-#         img_path = os.path.join(self.image_path, str(ID) + '.jpg').replace("\\","/")
-#         image = Image.open(img_path).convert('RGB')
-#         label = self.GT_data.iloc[idx, 2]
-#         text = self.GT_data.iloc[idx, 3] # text is not used in this example
-
-#         if self.transform:
-#             image = self.transform(image)
-
-#         # Tokenize text and get input IDs
-#         tokens = self.tokenizer.encode_plus(text, add_special_tokens=True, max_length=128, truncation=True, padding='max_length', return_tensors='pt')
-#         input_ids = tokens['input_ids']
-
-#         # Construct attention mask
-#         attention_mask = (input_ids != 0).float()
-
-#         # return image, input_ids, attention_mask,label
-#         return {
-#                     'input_ids': input_ids.flatten(),
-#                     'attention_mask': encoding['attention_mask'].flatten(),
-#                     'labels': torch.tensor(label, dtype=torch.long)
-#         }
-
 from torch.utils.data import Dataset, DataLoader
 import torch
 import pandas as pd
@@ -50,13 +13,13 @@ class MemeDataset(Dataset):
         self.GT_path = GT_path
         self.GT_data = pd.read_csv(GT_path)
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        self.max_len = 512
+        self.max_len = 256
 
     def __len__(self):
         return len(self.GT_data)
 
     def __getitem__(self, index):
-        text = self.GT_data.iloc[index, 3] # text is not used in this example
+        text = self.GT_data.iloc[index, 3]
         label = self.GT_data.iloc[index, 2]
         encoding = self.tokenizer.encode_plus(
             text,
@@ -68,8 +31,5 @@ class MemeDataset(Dataset):
             return_attention_mask=True,
             return_tensors='pt',
         )
-        return {
-            'input_ids': encoding['input_ids'].flatten(),
-            'attention_mask': encoding['attention_mask'].flatten(),
-            'labels': torch.tensor(label, dtype=torch.long)
-        }
+        return encoding['input_ids'].squeeze(0), encoding['attention_mask'].squeeze(0), torch.tensor([label], dtype=torch.float)
+        
--- vilbert_adapt.py
+++ vilbert_adapt.py
@@ -1,14 +1,20 @@
 import torch
 import torch.nn as nn
 import torchvision.models as models
-from transformers import BertModel, BertTokenizer, BertConfig
+from transformers import BertModel, VisualBertModel
 from transformers.models.bert.modeling_bert import BertPooler
 
 class MemeClassifier(nn.Module):
-    def __init__(self, bert_model='bert-base-uncased', num_labels=2):
+    def __init__(self, bert_model='bert-base-uncased', num_labels=1):
         super(MemeClassifier, self).__init__()
         self.bert = BertModel.from_pretrained(bert_model)  # Load pre-trained BERT
+
+        # Freeze BERT layers
+        for param in self.bert.parameters():
+            param.requires_grad = False
+
         self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)  # Classifier layer
+        self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_ids, attention_mask):
         # Forward pass through BERT
@@ -17,4 +23,5 @@ class MemeClassifier(nn.Module):
 
         # Pass through the classifier layer
         logits = self.classifier(pooled_output)
+        logits = self.sigmoid(logits)
         return logits
