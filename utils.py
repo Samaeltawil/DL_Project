3425279cd4d0c2168e75bfe71e400ba4e224b826
Adrien Pannatier <adrien.pannatier@epfl.ch>
2024-05-15 10:04:54 +0200

    Better visualizer

---
+++ utils.py
@@ -0,0 +1,49 @@
+from matplotlib import pyplot as plt
+
+# metrics = {'ACC': acc, 'F1-weighted': f1}
+# metrics_names = list(metrics.keys())
+
+def update_metrics_log(metrics_names, metrics_log, new_metrics_dict):
+    for i in range(len(metrics_names)):
+        curr_metric_name = metrics_names[i]
+        metrics_log[i].append(new_metrics_dict)
+    return metrics_log
+
+def train_cycle(model, optimizer, criterion, metrics, train_loader, test_loader, n_epochs, device):
+    train_loss_log,  test_loss_log = [], []
+    metrics_names = list(metrics.keys())
+    train_metrics_log = [[] for i in range(len(metrics))]
+    test_metrics_log = [[] for i in range(len(metrics))]
+
+    for epoch in range(n_epochs):
+        print("Epoch {0} of {1}".format(epoch, n_epochs))
+        train_loss, train_metrics = ... #train_epoch(model, optimizer, criterion, metrics, train_loader, device)
+
+        test_loss, test_metrics = ... #evaluate(model, criterion, metrics, test_loader, device)
+
+        train_loss_log.append(train_loss)
+        train_metrics_log = update_metrics_log(metrics_names, train_metrics_log, train_metrics)
+
+        test_loss_log.append(test_loss)
+        test_metrics_log = update_metrics_log(metrics_names, test_metrics_log, test_metrics)
+
+        plot_training(train_loss_log, test_loss_log, metrics_names, train_metrics_log, test_metrics_log)
+    return train_metrics_log, test_metrics_log
+
+def plot_training(train_loss, metrics_names, train_metrics_logs):
+    fig, ax = plt.subplots(1, len(metrics_names) + 1, figsize=((len(metrics_names) + 1) * 5, 5))
+
+    ax[0].plot(train_loss, c='blue', label='train')
+    # ax[0].plot(test_loss, c='orange', label='test')
+    ax[0].set_title('Loss')
+    ax[0].set_xlabel('epoch')
+    ax[0].legend()
+
+    for i in range(len(metrics_names)):
+        ax[i + 1].plot(train_metrics_logs[i], c='blue', label='train')
+        # ax[i + 1].plot(test_metrics_logs[i], c='orange', label='test')
+        ax[i + 1].set_title(metrics_names[i])
+        ax[i + 1].set_xlabel('epoch')
+        ax[i + 1].legend()
+
+    plt.show()
