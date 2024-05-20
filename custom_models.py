e790a1b85efb5a6fdb0174c234a299cf8d7d4de3
Yannis Paul Andre Coderey <coderey@izar1.izar.cluster>
2024-05-20 14:05:45 +0200

    modification of ResNet model

--- custom_models.py
+++ custom_models.py
@@ -6,68 +6,66 @@ from transformers import BertModel
 class CustomBert(nn.Module):
     def __init__(self, bert_model='bert-base-uncased', num_labels=1):
         super(CustomBert, self).__init__()
-        self.name = "CustomBert"
-        self.bert = BertModel.from_pretrained(bert_model)  # Load pre-trained BERT
-
+        self.bert = BertModel.from_pretrained(bert_model)
+        
         # Freeze BERT layers
         for param in self.bert.parameters():
             param.requires_grad = False
 
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)  # Classifier layer
+        self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, input_ids, attention_mask):
-        # Forward pass through BERT
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooled_output = outputs.pooler_output  # Get the pooled output from BERT
-
-        # Pass through the classifier layer
+        pooled_output = outputs.pooler_output
         logits = self.classifier(pooled_output)
-        logits = self.sigmoid(logits)
-        return logits
+        return self.sigmoid(logits)
 
 class ResNet_Bert(nn.Module):
-    def __init__(self, resnet_model = 'resnet18', bert_model='bert-base-uncased', num_labels = 1):
+    def __init__(self, resnet_model='resnet18', bert_model='bert-base-uncased', num_labels=1):
         super(ResNet_Bert, self).__init__()
-        self.name = "ResNet_Bert"
         self.resnet = torch.hub.load('pytorch/vision:v0.10.0', resnet_model, pretrained=True)
         self.resnet.eval()
-        self.res_num_features = 1000 # ResNet18
-        self.sigmoid = nn.Sigmoid()
+        self.res_num_features = 1000
 
-        self.bert = BertModel.from_pretrained(bert_model)  # Load pre-trained BERT
+        self.bert = BertModel.from_pretrained(bert_model)
         self.bert_num_features = 768
 
-        # Freeze BERT layers
+        # Freeze BERT and ResNet layers
         for param in self.bert.parameters():
             param.requires_grad = False
-
-        # Freeze ResNet layers
         for param in self.resnet.parameters():
             param.requires_grad = False
-        
+
         self.resnet_fc = nn.Linear(self.res_num_features, 512)
         self.bert_fc = nn.Linear(self.bert_num_features, 512)
 
-        self.classifier = nn.Linear(self.bert.config.hidden_size, num_labels)  # Classifier layer
+        self.dropout = nn.Dropout(p=0.5)
+        self.fc1 = nn.Linear(1024, 512)
+        self.bn1 = nn.BatchNorm1d(1024)
+        self.fc2 = nn.Linear(512, num_labels)
+        self.bn2 = nn.BatchNorm1d(512)
         self.sigmoid = nn.Sigmoid()
 
-        self.fc = nn.Linear(1024, 1)
-        
     def forward(self, input_ids, attention_mask, input_tensor_image):
-        res_outputs = self.resnet(input_tensor_image)
-        res_outputs = self.resnet_fc(res_outputs) # goes to 512 layers
+        with torch.no_grad():
+            res_outputs = self.resnet(input_tensor_image)
+        res_outputs = self.resnet_fc(res_outputs)
         res_outputs = F.relu(res_outputs)
+        res_outputs = self.dropout(res_outputs)
 
         bert_outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask)
-        pooled_output = bert_outputs.pooler_output  # Get the pooled output from BERT
+        pooled_output = bert_outputs.pooler_output
         bert_logits = self.bert_fc(pooled_output)
         bert_logits = F.relu(bert_logits)
+        bert_logits = self.dropout(bert_logits)
 
-        # concatenates the two outputs
         logits = torch.cat((res_outputs, bert_logits), 1)
-        logits = self.fc(logits)
+        logits = self.fc1(logits)
+        logits = self.bn1(logits)
+        logits = F.relu(logits)
+        logits = self.fc2(logits)
+        logits = self.bn2(logits)
         logits = self.sigmoid(logits)
 
         return logits
-    
